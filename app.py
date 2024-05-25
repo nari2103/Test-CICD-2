640f547c0c162f19ced9cbb182246ba9418065ce
nari2103 <168575884+nari2103@users.noreply.github.com>
2024-05-25 21:47:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+import psutil
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-   return "Hello World!"
-
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=8080)
+def index():
+    cpu_percent = psutil.cpu_percent()
+    mem_percent = psutil.virtual_memory().percent
+    Message = None
+    if cpu_percent > 80 or mem_percent > 80:
+        Message = "High CPU or Memory Detected, scale up!!!"
+    return render_template("index.html", cpu_percent=cpu_percent, mem_percent=mem_percent, message=Message)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
